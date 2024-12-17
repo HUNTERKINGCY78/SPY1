3e8de249c65ad51bd3302049c8ff50f0eec907cc
KING 👑 CY78 <hozoohunter@gmail.com>
2024-12-17 13:21:57 +0700

    Update ip.py

--- ip.py
+++ ip.py
@@ -19,53 +19,53 @@ def color_text(text):
 
 def ipsearch(ip_address):
     ip_info = get_ip_info(ip_address)
-    print(color_text("╔" + "─" * 65 + "╗"))
-    print(color_text("◆ ОБЩАЯ ИНФОРМАЦИЯ:"))
+    print (color_text("╔" + "─" * 65 + "╗"))
+    print (color_text("◆ INFORMASI UMUM:"))
 
     if 'ip' in ip_info:
-        print(color_text(f"◆ [+] IP-адрес: {ip_info['ip']}"))
+        print (color_text(f"◆ [+] IP-alamat: {ip_info['ip']}"))
         ip_type_info = get_ip_type(ip_address)
     else:
-        print(color_text("◆ [-] IP-адрес недоступен"))
+        print (color_text("◆ [-] IP-alamat tidak tersedia"))
 
-    print(color_text(f"◆ [+] Город: {ip_info.get('city', 'недоступен')}"))
-    print(color_text(f"◆ [+] Регион: {ip_info.get('region', 'недоступен')}"))
+    print(color_text(f"◆ [+] Kota: {ip_info.get('city', 'tidak tersedia')}"))
+    print(color_text(f"◆ [+] Wilayah: {ip_info.get('region', 'tidak tersedia')}"))
 
-    print(color_text(f"◆ [+] Континент: {ip_type_info['continent']} {ip_type_info['continent_code']}"))
-    print(color_text(f"◆ [+] Страна: {ip_type_info['country']} {ip_type_info['country_code']}"))
-    print(color_text(f"◆ [+] Почтовый Индекс: {ip_type_info['postal']}"))
-    print(color_text(f"◆ [+] Код Телефона: {ip_type_info['calling_code']}"))
-    print(color_text(f"◆ [+] Языки: {ip_type_info['borders']}"))
-    print(color_text(f"◆ [+] Часовой пояс: {ip_info.get('timezone', 'недоступен')}"))
+    print (color_text(f"◆ [+] Benua: {ip_type_info['continent']} {ip_type_info['continent_code']}"))
+    print (color_text(f"◆ [+] Negara: {ip_type_info['country']} {ip_type_info['country_code']}"))
+    print (color_text(f"◆ [+] Kode Pos: {ip_type_info['postal']}"))
+    print (color_text(f"◆ [+] Kode Pos Kode Telepon: {ip_type_info['calling_code']}"))
+    print (color_text(f"◆ [+] Bahasa: {ip_type_info['borders']}"))
+    print (color_text(f"◆ [+] BahasaZona waktu: {ip_info.get('timezone', 'недоступен')}"))
 
-    print(color_text("◆ ГЕОЛОКАЦИЯ:"))
+    print (color_text("◆ ГЕОЛОКАЦИЯ:"))
     if 'loc' in ip_info:
         latitude, longitude = ip_info['loc'].split(',')
-        print(color_text(f"◆ [+] Координаты: {ip_info['loc']}"))
+        print(color_text(f"◆ [+] Koordinat: {ip_info['loc']}"))
         google_maps_url = f"https://www.google.com/maps?q={latitude},{longitude}"
         yandex_maps_url = f"https://yandex.ru/maps/?ll={longitude},{latitude}&z=10"
-        print(color_text(f"◆ [+] Ссылка на Google Maps: {google_maps_url}"))
-        print(color_text(f"◆ [+] Ссылка на Yandex Maps: {yandex_maps_url}"))
+        print(color_text(f"◆ [+] Tautan ke Google Maps: {google_maps_url}"))
+        print(color_text(f"◆ [+] Tautan ke Yandex Maps: {yandex_maps_url}"))
     else:
-        print(color_text("◆ [-] Координаты недоступны"))
+        print(color_text("◆ [-] Koordinat tidak tersedia"))
 
-    print(color_text("◆ ТЕХНИЧЕСКАЯ ИНФОРМАЦИЯ:"))
-    print(color_text(f"◆ [+] Тип IP-адреса: {ip_type_info['type']}"))
-    print(color_text(f"◆ [+] Hostname: {ip_info.get('hostname', 'недоступен')}"))
-    print(color_text(f"◆ [+] Организация: {ip_info.get('org', 'недоступна')}"))
+    print (color_text("◆ INFORMASI TEKNIS:"))
+    print (color_text(f"◆ [+] Jenis alamat IP: {ip_type_info['type']}"))
+    print (color_text(f"◆ [+] Hostname: {ip_info.get('hostname', 'недоступен')}"))
+    print (color_text(f"◆ [+] Organisasi: {ip_info.get('org', 'недоступна')}"))
 
-    print(color_text("◆ ПОИСК IP В IoT:"))
+    print (color_text("◆ PENCARIAN IP DI IoT:"))
     shodan_url = f"https://www.shodan.io/search?query={ip_address}"
     censys_url = f"https://censys.io/ipv4/{ip_address}"
     zoomeye_url = f"https://www.zoomeye.org/searchResult?q={ip_address}"
     criminalip_url = f"https://www.criminalip.io/asset/report/{ip_address}"
     virustotal_url = f"https://www.virustotal.com/gui/ip-address/{ip_address}"
 
-    print(color_text(f"◆ [+] Shodan: {shodan_url}"))
-    print(color_text(f"◆ [+] Censys: {censys_url}"))
-    print(color_text(f"◆ [+] Zoomeye: {zoomeye_url}"))
-    print(color_text(f"◆ [+] CriminalIP: {criminalip_url}"))
-    print(color_text(f"◆ [+] VirusTotal: {virustotal_url}"))
+    print (color_text(f"◆ [+] Shodan: {shodan_url}"))
+    print (color_text(f"◆ [+] Censys: {censys_url}"))
+    print (color_text(f"◆ [+] Zoomeye: {zoomeye_url}"))
+    print (color_text(f"◆ [+] CriminalIP: {criminalip_url}"))
+    print (color_text(f"◆ [+] VirusTotal: {virustotal_url}"))
     print(color_text("╚" + "─" * 65 + "╝"))
 
 if __name__ == "__main__":
